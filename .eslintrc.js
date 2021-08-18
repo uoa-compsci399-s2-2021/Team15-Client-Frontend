4676a61a6d8dfa7bc0404037f3148e2f473ade82
Melo Guan <62586556+ygua699@users.noreply.github.com>
2021-08-18 23:38:15 +1200

    [CS399-005]Student : Client dashboard layout (#3)
    * added github action
    
    * added workflows
    
    * configured  workflows
    
    * changed yarn
    
    * add login page

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,5 +22,12 @@ module.exports = {
     'react/jsx-filename-extension': 'off',
     'react/react-in-jsx-scope': 'off',
     'no-console': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'jsx-a11y/anchor-is-valid': 'off',
+    'react/prop-types': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'react/jsx-curly-brace-presence': 'off',
+    'jsx-a11y/control-has-associated-label': 'off',
+    'no-unused-vars': 'off',
   },
 };
--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-import StyledButton from './components/StyledButton';
+import React, { useState, useEffect, useContext } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import PrivateRoute from './utils/PrivateRoute';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
   return (
+
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <StyledButton />
-
+        <Router>
+          <Switch>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <PrivateRoute path="/app">
+              <Home />
+            </PrivateRoute>
+          </Switch>
+        </Router>
       </header>
     </div>
+
   );
 }
 
